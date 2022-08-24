dcd358c17c32d4968bc5b7dd91fdb55042ed3c93
Victor Rodriguez <victor_jacholive@hotmail.com>
2022-08-23 23:05:37 -0500

    primera version

--- mayorDeCuatroNumeros.py
+++ mayorDeCuatroNumeros.py
@@ -1,6 +1,18 @@
 
 def main():
-    pass
+    N = list(input('ingresa 4 numeros cualesquiera: '))
+    print(f'Escribiste los siguientes numeros {N}')
 
+    if len(N) == 4:
+        if (N[0] > N[1] and (N[0] > N[2]) and (N[0] > N[3])):
+            print(f'{N[0]} es el mayor de los 4 numeros')
+        elif (N[1] > N[0] and (N[1] > N[2]) and (N[1] > N[3])):
+            print(f'{N[1]} es el mayor de los 4 numeros')
+        elif (N[2] > N[0] and (N[2] > N[1]) and (N[2] > N[3])):
+            print(f'{N[2]} es el mayor de los 4 numeros')
+        elif (N[3] > N[0] and (N[3] > N[1]) and (N[3] > N[2])):
+            print(f'{N[3]} es el mayor de los 4 numeros')  
+    else:
+        print('Ingresa solo 4 numeros')
 if __name__=='__main__':
     main()
