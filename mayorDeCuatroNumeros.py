98f6f68bef9d602984422bb66ea9e68707deaba9
Victor Rodriguez <victor_jacholive@hotmail.com>
2022-08-24 22:51:39 -0500

    segunda version

--- mayorDeCuatroNumeros.py
+++ mayorDeCuatroNumeros.py
@@ -1,18 +1,21 @@
 
 def main():
-    N = list(input('ingresa 4 numeros cualesquiera: '))
-    print(f'Escribiste los siguientes numeros {N}')
+    #try:
+        N = input(f'ingresa 4 numeros cualesquiera: ')
+        delimiter = ','
+        numero = N.split(delimiter)
+        numero.sort()
+        #print(f'Escribiste los siguientes numeros {numero}')
 
-    if len(N) == 4:
-        if (N[0] > N[1] and (N[0] > N[2]) and (N[0] > N[3])):
-            print(f'{N[0]} es el mayor de los 4 numeros')
-        elif (N[1] > N[0] and (N[1] > N[2]) and (N[1] > N[3])):
-            print(f'{N[1]} es el mayor de los 4 numeros')
-        elif (N[2] > N[0] and (N[2] > N[1]) and (N[2] > N[3])):
-            print(f'{N[2]} es el mayor de los 4 numeros')
-        elif (N[3] > N[0] and (N[3] > N[1]) and (N[3] > N[2])):
-            print(f'{N[3]} es el mayor de los 4 numeros')  
-    else:
-        print('Ingresa solo 4 numeros')
+        salida = numero[3]
+        print(f'El {salida} es el mayor')
+
+        #for i in numero:
+        #    #print(i)
+        #    if i >= N[0] and >= N[1] >= N[2] >= N[3]:
+        #        print(f'{i} es mayor')
+       
+    #except:
+    #    print('Ingresa solo numeros')
 if __name__=='__main__':
     main()
